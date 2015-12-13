9de06fdb4783a5c7847043dd3746a9514684e7f6
root <root@localhost.localdomain>
2015-12-13 23:37:45 +0800

    add ueditor

--- App/Config/route.php
+++ App/Config/route.php
@@ -7,6 +7,7 @@ if (!defined('BASEDIR')) {
 
 // 任务api
 $app->get('/admin', ['as' => 'task_index', 'uses' => 'App\Controller\Admin\IndexController@index']);
-$app->get('/admin/post', ['as' => 'task_index', 'uses' => 'App\Controller\Admin\IndexController@index']);
+$app->get('/admin/post', ['as' => 'task_index', 'uses' => 'App\Controller\Admin\PostController@index']);
+$app->get('/admin/post_add', ['as' => 'task_index', 'uses' => 'App\Controller\Admin\PostController@add']);
 $app->get('/admin/medal', ['as' => 'task_index', 'uses' => 'App\Controller\Admin\MedalController@index']);
 $app->get('/admin/medal_upload', ['as' => 'task_index', 'uses' => 'App\Controller\Admin\MedalController@medalUpload']);
