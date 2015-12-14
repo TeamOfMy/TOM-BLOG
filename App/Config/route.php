8feb8eb38009daec5aff4822e6825e2163cc2310
halin <lwl@outlook.com>
2015-12-15 01:24:10 +0800

    add upload

--- App/Config/route.php
+++ App/Config/route.php
@@ -10,7 +10,10 @@ $app->get('/admin', ['as' => 'task_index', 'uses' => 'App\Controller\Admin\Index
 $app->get('/admin/post', ['as' => 'post', 'uses' => 'App\Controller\Admin\PostController@index']);
 $app->get('/admin/post_add', ['as' => 'post_add', 'uses' => 'App\Controller\Admin\PostController@add']);
 $app->post('/admin/post_add_data', ['as' => 'post_add_data', 'uses' => 'App\Controller\Admin\PostController@addData']);
+
 $app->get('/admin/medal', ['as' => 'task_index', 'uses' => 'App\Controller\Admin\MedalController@index']);
 $app->get('/admin/medal_upload', ['as' => 'task_index', 'uses' => 'App\Controller\Admin\MedalController@medalUpload']);
+$app->post('/admin/medal_upload_file', ['as' => '', 'uses' => 'App\Controller\Admin\MedalController@upload']);
+
 $app->get('/admin/login', ['as' => 'login_index', 'uses' => 'App\Controller\Admin\PublicController@login']);
 $app->post('/admin/check_login', ['as' => 'check_login', 'uses' => 'App\Controller\Admin\PublicController@checkLogin']);
